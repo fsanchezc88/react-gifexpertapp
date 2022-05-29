bd1177d315bf1ef8ba07138b70a8d91378b3269c
Faure <fsanchezc88@gmail.com>
2022-05-29 00:45:29 -0400

    Mostrar los titulos de las Imagenes.

--- src/components/GifGtid.js
+++ src/components/GifGtid.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGtid = ({ category }) => {
 
-  const [count, setCount] = useState(0);
+  const [images, setImages] = useState([])
 
   // Segundo argumento del useEffect es un arreglo de dependencias.
   // 2do arg vacio [] : componentDidMount(); solo se ejecuta la vez q se monta el componente. 
@@ -22,18 +23,36 @@ export const GifGtid = ({ category }) => {
         title: img.title,
         url: img.images?.downsized_medium.url
       }
-    })
+    });
 
     console.log(gifs);
+    setImages(gifs);
 
   }
 
+  const liImages = images.map( img => {
+    return (<li key={ img.id } >{ img.title }</li>);
+  });
+  
+  /*const liImagesx = images.map( ({id, title }) => {
+    return (<li key={ id } >{ title }</li>);
+  });*/
 
   return (
     <div>
         <h3> { category } </h3>
-        <h3> { count } </h3>
-        <button onClick={ () => setCount ( count + 1 ) }>X</button>
+        <ol>
+        { /*liImages*/ }
+        {
+          images.map( ( img ) => (
+            <GifGridItem 
+              key={ img.id }
+              /*img = { img }*/
+              { ...img }
+            />
+          ))
+        }
+        </ol>
     </div>
   )
 }
