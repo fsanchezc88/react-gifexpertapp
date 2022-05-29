bf6f7f4d340973346c3592b6dd23cb8b00c53a96
Faure <fsanchezc88@gmail.com>
2022-05-28 23:40:06 -0400

    useEffect().

--- src/components/GifGtid.js
+++ src/components/GifGtid.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const GifGtid = ({ category }) => {
+
+  const [count, setCount] = useState(0);
+
+  // Segundo argumento del useEffect es un arreglo de dependencias.
+  // 2do arg vacio [] : componentDidMount(); solo se ejecuta la vez q se monta el componente. 
+  useEffect( () => {
+    getGifs();
+  }, []); 
 
   const getGifs = async() => {
 
@@ -20,12 +28,12 @@ export const GifGtid = ({ category }) => {
 
   }
 
-  getGifs();
-
 
   return (
     <div>
         <h3> { category } </h3>
+        <h3> { count } </h3>
+        <button onClick={ () => setCount ( count + 1 ) }>X</button>
     </div>
   )
 }
